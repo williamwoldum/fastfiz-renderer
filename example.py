2d3ebbe8bbfeb11af688d9ba09fcea0333a5c250
williamwoldum <wwoldu21@student.aau.dk>
2024-03-04 11:01:24 +0100

    Added example

---
+++ example.py
@@ -0,0 +1,16 @@
+from fastfiz_renderer import DevUtils, GameHandler
+import os
+
+
+def main():
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "100, 100"
+
+    shot_decider = DevUtils.DevShotDeciders.get_north_shot_decider
+
+    game_handler = GameHandler()
+    game_handler.play_eight_ball(shot_decider, frames_per_second=120, scale=300)
+    game_handler.play_game_from_table_state(DevUtils.DevTableStates.get_one_ball_state(), shot_decider)
+
+
+if __name__ == '__main__':
+    main()
