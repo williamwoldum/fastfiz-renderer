c193963e6b90760481bb719cd65a197205f46381
williamwoldum <wwoldu21@student.aau.dk>
2024-03-04 11:56:52 +0100

    Moved scaling to gamehandler

--- example.py
+++ example.py
@@ -3,12 +3,10 @@ import os
 
 
 def main():
-    os.environ['SDL_VIDEO_WINDOW_POS'] = "100, 100"
-
     shot_decider = DevUtils.DevShotDeciders.get_north_shot_decider
 
-    game_handler = GameHandler()
-    game_handler.play_eight_ball(shot_decider, frames_per_second=120, scale=300)
+    game_handler = GameHandler(window_pos=(300, 200), frames_per_second=120, scale=300)
+    game_handler.play_eight_ball(shot_decider)
     # game_handler.play_game_from_table_state(DevUtils.DevTableStates.get_one_ball_state(), shot_decider)
 
 
--- fastfiz_renderer/GameHandler.py
+++ fastfiz_renderer/GameHandler.py
@@ -8,42 +8,42 @@ from .GameTable import GameTable
 
 class GameHandler:
     _instance = None
-    _DEFAULT_SCALE = 400
-    _DEFAULT_FRAMES_PER_SECOND = 60
 
-    def __init__(self, mac_mode=False):
+    def __init__(self, mac_mode=False, window_pos=(100, 100), frames_per_second=60, scale=200):
         if GameHandler._instance is None:
             self._table_state: Optional[ff.TableState] = None
             self._game_table: Optional[GameTable] = None
             self._start_ball_positions: dict[int, Tuple[float, float]] = dict()
             self._shot_decider: Optional[Callable[[ff.TableState], ff.ShotParams]] = None
-            self._mac_mode = mac_mode
+            self._mac_mode: bool = mac_mode
+            self._window_pos: Tuple[int, int] = window_pos
+            self._scale: int = scale
+            self._frames_per_second: int = frames_per_second
+
             GameHandler._instance = self
         else:
             raise Exception("This class is a singleton!")
 
-    def play_eight_ball(self, shot_decider: Callable[[ff.TableState], ff.ShotParams], scale: int = _DEFAULT_SCALE,
-                        frames_per_second: int = _DEFAULT_FRAMES_PER_SECOND):
+    def play_eight_ball(self, shot_decider: Callable[[ff.TableState], ff.ShotParams]):
         game_state: ff.GameState = ff.GameState.RackedState(ff.GT_EIGHTBALL)
         table_state: ff.TableState = game_state.tableState()
-        self.play_game_from_table_state(table_state, shot_decider, scale, frames_per_second)
+        self.play_game_from_table_state(table_state, shot_decider)
 
     def play_game_from_table_state(self, table_state: ff.TableState,
-                                   shot_decider: Callable[[ff.TableState], ff.ShotParams], scale: int = _DEFAULT_SCALE,
-                                   frames_per_second: int = _DEFAULT_FRAMES_PER_SECOND):
+                                   shot_decider: Callable[[ff.TableState], ff.ShotParams]):
         self._table_state = table_state
         self._game_table = GameTable.from_table_state(table_state)
         self._load_start_balls()
         self._shot_decider = shot_decider
 
         def _setup():
-            size(int(self._game_table.width * scale), int(self._game_table.length * scale))
+            size(int(self._game_table.width * self._scale), int(self._game_table.length * self._scale))
             ellipseMode(CENTER)
             noStroke()
 
         def _draw():
             self._game_table.update()
-            self._game_table.draw(scale * 2 if self._mac_mode else scale)
+            self._game_table.draw(self._scale * 2 if self._mac_mode else self._scale)
 
         def _key_released(event):
             if event.key == "RIGHT":
@@ -51,8 +51,9 @@ class GameHandler:
             elif event.key == "r" or event.key == "R":
                 self._handle_restart()
 
-        run(renderer="skia", frame_rate=frames_per_second, sketch_draw=_draw, sketch_setup=_setup,
-            sketch_key_released=_key_released)
+        run(renderer="skia", frame_rate=self._frames_per_second, sketch_draw=_draw, sketch_setup=_setup,
+            sketch_key_released=_key_released, window_xpos=self._window_pos[0], window_ypos=self._window_pos[1],
+            window_title="Pool renderer")
 
     def _handle_restart(self):
         for ball_number, pos in self._start_ball_positions.items():
